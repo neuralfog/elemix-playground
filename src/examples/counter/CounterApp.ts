2494c8b51350cf8e482a9dc9f2913ca64554e66c
brownhounds <brownhounds739@gmail.com>
2026-07-22 23:53:32 +0100

    v0.9.0-dev.27

--- src/examples/counter/CounterApp.ts
+++ src/examples/counter/CounterApp.ts
@@ -3,23 +3,19 @@ import type { Template } from '@neuralfog/elemix/types';
 
 import css from './CounterApp.scss?inline';
 
-type State = {
-    count: number;
-};
-
 // #component
 export class CounterApp extends Component {
     // #styles
     styles = css;
 
     // #state
-    state: State = { count: 0 };
+    count = 0;
 
     increment = (): void => {
-        this.state.count++;
+        this.count++;
     };
 
     template = (): Template => tpl`
-        <button @click=${this.increment}>count is ${this.state.count}</button>
+        <button @click=${this.increment}>count is ${this.count}</button>
     `;
 }
